6e51eedd1727c913fad1d352d8c97cba4b8b5fd3
Sathsarani H E S <99157473+IT21266546@users.noreply.github.com>
2022-02-16 05:11:38 +0000

    C TO CPP CONVERT

--- tute04.cpp
+++ tute04.cpp
@@ -22,8 +22,29 @@ int main() {
   std::cin >> n;
   std::cout << "Enter a value for r ";
   std::cin >> r;
+  
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+long Factorial(int no)
+{
+  long fac = 1; 
+
+  for(int r = no ; r >= 1 ; r--)
+  {
+     fac = fac * r;
+  }
+  
+  return fac;
+  
+}
+long nCr(int n, int r)
+{
+  long NCR = 0 ; 
+
+  NCR =Factorial(n) / (Factorial(r) * Factorial((n-r)));
+
+  return NCR;
 }
